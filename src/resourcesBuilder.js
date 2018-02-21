43f32e7880771f8a9351dd2115ffeb552f838650
k911 <konradobal@gmail.com>
2018-02-21 21:19:25 +0100

    simplify resourcesBuilder 3

--- src/resourcesBuilder.js
+++ src/resourcesBuilder.js
@@ -7,39 +7,43 @@ import defaultInputFactory from './inputFactory';
 import List from './List';
 import Show from './Show';
 
+const makeResource = (resource, api, fieldFactory, inputFactory) => {
+  const {
+    create = Create,
+    edit = Edit,
+    list = List,
+    name,
+    props,
+    remove = Delete,
+    show = Show,
+  } = resource;
+  return (
+    <Resource
+      {...props}
+      create={create}
+      edit={edit}
+      key={name}
+      list={list}
+      name={name}
+      options={{
+        api,
+        fieldFactory,
+        inputFactory,
+        resource,
+      }}
+      remove={remove}
+      show={show}
+    />
+  );
+};
+
 const resourcesBuilder = (
   api,
   fieldFactory = defaultFieldFactory,
   inputFactory = defaultInputFactory,
 ) =>
-  api.resources.map(resource => {
-    const {
-      create = Create,
-      edit = Edit,
-      list = List,
-      name,
-      props,
-      remove = Delete,
-      show = Show,
-    } = resource;
-    return () => (
-      <Resource
-        {...props}
-        create={create}
-        edit={edit}
-        key={name}
-        list={list}
-        name={name}
-        options={{
-          api,
-          fieldFactory,
-          inputFactory,
-          resource,
-        }}
-        remove={remove}
-        show={show}
-      />
-    );
-  });
+  api.resources.map(resource =>
+    makeResource(resource, api, fieldFactory, inputFactory),
+  );
 
 export default resourcesBuilder;
